8ce256629775d7888b7d5d61c51791a9592f9cef
Yugander-yadav <yuganderpilli@outlook.com>
2022-06-01 18:10:25 +0530

    hello world

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const mongodb=require("./connect")
 mongodb.connect()
 const app=express()
 app.use(express.json())
+
 app.use("/",(req,res,next)=>{
    console.log("server started")
    
+
+
+
     next()
 })
 
